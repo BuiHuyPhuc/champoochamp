52cfd0993e8d6175ce4a89bcaca7e5b298a0ac65
Phong Phu <phuphamphong97@gmail.com>
2019-12-03 00:51:59 +0700

    Complete UI for Register page.

--- Champoochamp/ClientApp/src/components/RegisterPage/RegisterPage.js
+++ Champoochamp/ClientApp/src/components/RegisterPage/RegisterPage.js
@@ -1,8 +1,45 @@
 ﻿import React, { Component } from 'react';
-import { Form, Icon, Input, Button } from 'antd';
+import { NavLink } from 'react-router-dom';
+import { Row, Col, Form, Input } from 'antd';
+import styled from '@emotion/styled';
 
-import { callAPI, setCookie } from '../../shared/utils';
-import { emailKey, passwordKey } from '../../shared/constants';
+import {
+  callAPI,
+  setCookie,
+  formatForm,
+  formatCheckbox
+} from '../../shared/utils';
+import { emailKey, passwordKey, viewportWidth } from '../../shared/constants';
+import { colors } from '../../shared/principles';
+
+import { PageContainer, Button, SectionTitle, Link } from '../elements';
+
+const Wrapper = styled('div')`
+  ${formatForm};
+  ${formatCheckbox};
+  display: flex;
+  height: 80vh;
+`;
+
+const LoginForm = styled(Form)`
+  margin: auto;
+  max-width: ${viewportWidth.sm}px;
+  width: 100%;
+`;
+
+const RegisterContainer = styled('div')`
+  display: flex;
+  margin-bottom: 15px;
+`;
+
+const RegisterText = styled('span')`
+  color: ${colors.darkGray};
+  margin-right: 5px;
+`;
+
+const LoginButton = styled(Button)`
+  margin-bottom: 15px;
+`;
 
 class RegisterPage extends Component {
   onSubmit = e => {
@@ -16,95 +53,117 @@ class RegisterPage extends Component {
             setCookie(emailKey, res.data.email, 1);
             setCookie(passwordKey, res.data.password, 1);
             history.push('/');
-          }
-          else {
-            alert("Đăng ký thất bại!");
+          } else {
+            alert('Đăng ký thất bại!');
           }
         });
       }
     });
-  }
+  };
 
   render() {
     const { getFieldDecorator } = this.props.form;
 
     return (
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <Form onSubmit={this.onSubmit}>
-          <Form.Item>
-            {getFieldDecorator('email', {
-              rules: [{ required: true, message: 'Vui lòng nhập email!' }]
-            })(
-              <Input placeholder="email" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Vui lòng nhập mật khẩu!' }]
-            })(
-              <Input type="password" placeholder="Mật khẩu" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('firstName', {
-              rules: [{ required: true, message: 'Vui lòng nhập họ!' }]
-            })(
-              <Input placeholder="Họ" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('lastName', {
-              rules: [{ required: true, message: 'Vui lòng nhập tên!' }]
-            })(
-              <Input placeholder="Tên" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('telephone', {
-              rules: [{ required: true, message: 'Vui lòng nhập số điện thoại!' }]
-            })(
-              <Input placeholder="Số điện thoại" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('province', {
-              rules: [{ required: true, message: 'Vui lòng nhập tỉnh!' }]
-            })(
-              <Input placeholder="Tỉnh" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('district', {
-              rules: [{ required: true, message: 'Vui lòng nhập huyện!' }]
-            })(
-              <Input placeholder="Quận" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('ward', {
-              rules: [{ required: true, message: 'Vui lòng nhập đường!' }]
-            })(
-              <Input placeholder="Đường" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('address', {
-              rules: [{ required: true, message: 'Vui lòng nhập số nhà!' }]
-            })(
-              <Input placeholder="Số nhà" />
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Đăng ký
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+      <PageContainer>
+        <Wrapper>
+          <LoginForm onSubmit={this.onSubmit}>
+            <SectionTitle content="Đăng kí" />
+            <RegisterContainer>
+              <RegisterText>Đã có tài khoản?</RegisterText>
+              <NavLink to="/dang-nhap">
+                <Link content="Đăng nhập" />
+              </NavLink>
+            </RegisterContainer>
+
+            <Form.Item>
+              {getFieldDecorator('fullName', {
+                rules: [{ required: true, message: 'Vui lòng nhập họ tên!' }]
+              })(<Input placeholder="Họ tên *" />)}
+            </Form.Item>
+
+            <Row gutter={16}>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('email', {
+                    rules: [{ required: true, message: 'Vui lòng nhập email!' }]
+                  })(<Input placeholder="Email *" />)}
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('phone', {
+                    rules: [
+                      {
+                        required: true,
+                        message: 'Vui lòng nhập số điện thoại!'
+                      }
+                    ]
+                  })(<Input placeholder="Số điện thoại *" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={16}>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('password', {
+                    rules: [
+                      { required: true, message: 'Vui lòng nhập mật khẩu!' }
+                    ]
+                  })(<Input type="password" placeholder="Mật khẩu *" />)}
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('rePassword', {
+                    rules: [
+                      { required: true, message: 'Vui lòng nhập lại mật khẩu!' }
+                    ]
+                  })(
+                    <Input type="password" placeholder="Nhập lại mật khẩu *" />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={16}>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('province', {
+                    rules: [{ message: 'Vui lòng nhập tỉnh!' }]
+                  })(<Input placeholder="Tỉnh / thành phố" />)}
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('district', {
+                    rules: [{ message: 'Vui lòng nhập huyện!' }]
+                  })(<Input placeholder="Quận / huyện" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Row gutter={16}>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('ward', {
+                    rules: [{ message: 'Vui lòng nhập phường!' }]
+                  })(<Input placeholder="Phường" />)}
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item>
+                  {getFieldDecorator('address', {
+                    rules: [{ message: 'Vui lòng nhập số nhà, đường!' }]
+                  })(<Input placeholder="Số nhà, đường" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <LoginButton title="Đăng kí" htmlType="submit" isBlockButton />
+          </LoginForm>
+        </Wrapper>
+      </PageContainer>
     );
   }
 }
